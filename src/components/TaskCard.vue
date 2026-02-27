0f173948ad863e87711b8b246e37bc0a29fed636
soknanprogramming <poch.soknan@gmail.com>
2026-02-28 06:48:01 +0700

    make card

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import type { Task } from '@/types/Task';
+defineProps<{
+  task: Task;
+  index: number;
+}>()
+
+defineEmits<{
+  (e: 'complete', index: number): void;
+  (e: 'delete', index: number): void;
+}>()
 </script>
 
 <template>
-  <div ref="container" class="border-green h-48 w-64 rounded">
-    heo
-
+  <div ref="container" class="border-green h-48 w-64 rounded bg-amber-400 m-4 p-2">
+    <strong>{{ task.name || 'No title' }}</strong>
+    <p>{{ task.description || 'No description' }}</p>
   </div>
 </template>
--- src/views/ViewTasks.vue
+++ src/views/ViewTasks.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import TaskCard from '@/components/TaskCard.vue';
+import { useTasksStore } from '@/stores/tasks';
+const tasksStore = useTasksStore();
+
+
 </script>
 
 <template>
   <div>
-    <TaskCard />
+    <TaskCard v-for="(task, index) in tasksStore.tasks" :key="index" :index="index" :task="task" />
   </div>
 </template>
