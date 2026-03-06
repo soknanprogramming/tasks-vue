364ffeda6ea47b830c3a919fc0bd66534e9bca6a
soknanprogramming <poch.soknan@gmail.com>
2026-03-06 07:30:27 +0700

    Updates

---
+++ assets/AboutView-CvKNjoMx.js
@@ -0,0 +1,1 @@
+import{_ as t,o as s,c as o,a}from"./index-eja4aKU6.js";const n={},c={class:"text-white text-center text-9xl"};function r(i,e){return s(),o("div",c,[...e[0]||(e[0]=[a("h1",null,"This is an about page",-1)])])}const l=t(n,[["render",r]]);export{l as default};
