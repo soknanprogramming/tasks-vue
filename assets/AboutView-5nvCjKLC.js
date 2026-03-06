5ec1d2191df6aad03f0520f908d680ffdf0d1cb8
soknanprogramming <poch.soknan@gmail.com>
2026-03-06 20:52:24 +0700

    Updates

--- assets/AboutView-5nvCjKLC.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as s,c as o,a}from"./index-DqbLogqn.js";const n={},c={class:"text-white text-center text-9xl"};function r(i,e){return s(),o("div",c,[...e[0]||(e[0]=[a("h1",null,"This is an about page",-1)])])}const l=t(n,[["render",r]]);export{l as default};
